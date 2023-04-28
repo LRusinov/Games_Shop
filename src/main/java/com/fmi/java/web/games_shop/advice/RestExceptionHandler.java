0550f821835c8d3d2875e2039fcd81b9a3fed47d
LRusinov <lubomir202020@gmail.com>
2023-04-28 19:47:59 +0300

    Added exception handler for EntityExistException.

---
+++ src/main/java/com/fmi/java/web/games_shop/advice/RestExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.fmi.java.web.games_shop.advice;
+
+import com.fmi.java.web.games_shop.dto.Error;
+import com.fmi.java.web.games_shop.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleNotFound(
+            RuntimeException ex, WebRequest request) {
+        String message = ex.getMessage();
+        Error error = new Error(message);
+        return handleExceptionInternal(ex, error,
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
---
+++ src/main/java/com/fmi/java/web/games_shop/dto/Error.java
@@ -0,0 +1,4 @@
+package com.fmi.java.web.games_shop.dto;
+
+public record Error(String message) {
+}
