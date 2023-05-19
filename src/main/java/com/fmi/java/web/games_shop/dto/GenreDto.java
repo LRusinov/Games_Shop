8402796d95d693b61a4cf2f930ac6f23e44958cc
LRusinov <lubomir202020@gmail.com>
2023-05-20 01:57:17 +0300

    Added DTOs.

---
+++ src/main/java/com/fmi/java/web/games_shop/dto/GenreDto.java
@@ -0,0 +1,4 @@
+package com.fmi.java.web.games_shop.dto;
+
+public record GenreDto(String name) {
+}
---
+++ src/main/java/com/fmi/java/web/games_shop/dto/PublisherDto.java
@@ -0,0 +1,4 @@
+package com.fmi.java.web.games_shop.dto;
+
+public record PublisherDto(Long id, String name, String logoPictureUrl, int yearOfCreation, String description) {
+}
