69c2d73a35cf47ddf581e4126f990f23dea18099
LRusinov <lubomir202020@gmail.com>
2023-05-25 00:58:37 +0300

    Updated creta and view piblishers.

--- src/main/resources/webapp/games-shop-app/src/app/module/publisher/components/view-publisher/view-publisher.component.ts
+++ src/main/resources/webapp/games-shop-app/src/app/module/publisher/components/view-publisher/view-publisher.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Publisher } from 'src/app/model/Publisher';
+import { PublisherService } from '../../services/publisher.service';
 
 @Component({
   selector: 'app-view-publisher',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-publisher.component.css']
 })
 export class ViewPublisherComponent {
+publishers:Publisher[]=[];
+displayedColumns:string[]=[    'name','yearOfCreation','description','logoPictureUrl'];
+public dataSource = new MatTableDataSource<Publisher>();
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort();
 
+  constructor(
+    private readonly publisherService: PublisherService,
+  ) {}
+
+  ngOnInit(): void {
+    this.publisherService.getPublishers().subscribe((response) => {
+      this.publishers = response;
+      this.dataSource.data = this.publishers;
+      this.dataSource.sort = this.sort;
+    });
+  }
 }
--- src/main/resources/webapp/games-shop-app/src/app/module/publisher/services/publisher.service.ts
+++ src/main/resources/webapp/games-shop-app/src/app/module/publisher/services/publisher.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { Publiher } from 'src/app/model/Publisher';
+import { Publisher } from 'src/app/model/Publisher';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +10,8 @@ export class PublisherService {
 
   constructor(private readonly httpClient: HttpClient) {}
 
-  getPublishers(): Observable<Publiher[]> {
-    return this.httpClient.get<Publiher[]>(
+  getPublishers(): Observable<Publisher[]> {
+    return this.httpClient.get<Publisher[]>(
       'http://localhost:8080/games-shop/publishers'
     );
   }
@@ -21,8 +21,8 @@ export class PublisherService {
     yearOfCreation: number,
     description: string,
     logoPictureUrl: string
-  ): Observable<Publiher> {
-    return this.httpClient.post<Publiher>(
+  ): Observable<Publisher> {
+    return this.httpClient.post<Publisher>(
       'http://localhost:8080/games-shop/publishers',
       {
         name,
