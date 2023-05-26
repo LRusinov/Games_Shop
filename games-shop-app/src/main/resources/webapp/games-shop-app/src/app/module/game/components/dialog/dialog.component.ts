dd2bdc5263d7238cce37187e4d12161e4bd44398
LRusinov <lubomir202020@gmail.com>
2023-05-27 01:09:43 +0300

    Updated dialog outlook.

--- games-shop-app/src/main/resources/webapp/games-shop-app/src/app/module/game/components/dialog/dialog.component.ts
+++ games-shop-app/src/main/resources/webapp/games-shop-app/src/app/module/game/components/dialog/dialog.component.ts
@@ -4,6 +4,9 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { GameService } from '../../services/game.service';
 import { Game } from 'src/app/model/Game';
 import { CreateGameComponent } from '../create-game/create-game.component';
+import { PlatformService } from 'src/app/module/platform/services/platform.service';
+import { GenreService } from 'src/app/module/genre/services/genre.service';
+import { PublisherService } from 'src/app/module/publisher/services/publisher.service';
 export interface DialogData {
   animal: string;
 }
@@ -14,12 +17,21 @@ export interface DialogData {
 })
 export class DialogComponent extends CreateGameComponent {
   constructor(
+    private readonly platformService_: PlatformService,
+    private readonly genreService_: GenreService,
+    private readonly publisherService_: PublisherService,
     private readonly gameService_: GameService,
     private readonly formBuilder_: FormBuilder,
     public dialogRef: MatDialogRef<DialogComponent>,
     @Inject(MAT_DIALOG_DATA) public game: Game
   ) {
-    super(gameService_, formBuilder_);
+    super(
+      platformService_,
+      genreService_,
+      publisherService_,
+      gameService_,
+      formBuilder_
+    );
   }
 
   override ngOnInit(): void {
