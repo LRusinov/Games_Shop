53afd62933f758b6610a9231bccea48b097e4b27
LRusinov <lubomir202020@gmail.com>
2023-06-12 19:20:22 +0300

    Added test class for Genre service.

---
+++ games-shop-app/src/test/java/com/fmi/java/web/games_shop/service/GenreServiceTest.java
@@ -0,0 +1,25 @@
+package com.fmi.java.web.games_shop.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenreServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllGenres() {
+    }
+
+    @Test
+    void addGenre() {
+    }
+
+    @Test
+    void deleteGenre() {
+    }
+}
